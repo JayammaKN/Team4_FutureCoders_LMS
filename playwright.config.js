a4c50ce5396acbe8f2df7eee7db01fa014d1c001
Jayamma K N <jayammakn411@github-email.com>
2026-08-11 09:34:03 -0700

    Capture video on test failure

--- playwright.config.js
+++ playwright.config.js
@@ -38,6 +38,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'retain-on-failure',
     screenshot: 'only-on-failure',
     headless: true,
   },
